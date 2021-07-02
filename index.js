212608bcdf29911e093e19bba810bcffb43ae343
Luca Cariolin <lucacariolin@hotmail.com>
2021-07-01 22:10:36 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const routes = require('./routes/routes')
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger_arquivo.json');
+
+require('dotenv').config({ path: './config/.env'});
+
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+
+app.use('/documentacao', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
+app.use(routes);
+app.listen(3000);
+
---
+++ swagger.js
@@ -0,0 +1,43 @@
+const swaggerAutogen = require('swagger-autogen')()
+
+const outputFile = './swagger_arquivo.json'
+const routes = ['./index.js'];
+const docs = {
+    "swagger": "2.0",
+    "info": {
+      "version": "1.0.0",
+      "title": "API de Exemplo de Documentação",
+      "description": "Esta é uma api documentada"
+    },
+    "host": "localhost:3000",
+    "basePath": "/",
+    "tags": [],
+    "schemes": [
+      "http",
+      "https"
+    ],
+    "consumes": ["application/json"],
+    "produces": ["application/json"],
+    "securityDefinitions": {
+        "apiKeyAuth": {
+          
+        }
+    },
+    "definitions": {
+        Time: {
+            nome: "Cruzeirão Cabuloso",
+            sede: "Toca 3"
+        },
+        Pessoa: {
+            nome: "John Snow",
+            serie: "GoT",
+            time: {
+                $ref: '#/definitions/Time'
+            },
+        }
+    }
+};
+
+swaggerAutogen(outputFile, routes, docs).then(() => {
+    require('./index.js')
+})
